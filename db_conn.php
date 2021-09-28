f0d3d25fb193cf6885f597d1426f128724bd8678
Gabe Santos <gabesantos726@gmail.com>
2021-09-28 17:15:32 +0800

    Modified small changes

--- db_conn.php
+++ db_conn.php
@@ -1,12 +1,12 @@
 <?php
-
 $server_name = "127.0.0.1:3306"; // put your Server 
-$user_name = "root"; //USername 
+$user_name = "root"; // username 
 $password = ""; // enter password
-$Db_name = "ecommerce_project"; // Database Name
+$Db_name = ""; // database Name
 
 $conn = mysqli_connect($server_name, $user_name, $password, $Db_name);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+?>
